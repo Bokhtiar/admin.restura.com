db087795b5eafac62b60e17d3104f5518404db67
Bokhtiar Tushar <bokhtiartoshar1@gmail.com>
2023-03-06 23:49:19 +0600

    category list desing with react-data-table-component done

--- src/Pages/Category/index.tsx
+++ src/Pages/Category/index.tsx
@@ -1,7 +1,58 @@
-import {Header} from '../../Layouts/Header/index'
+import { useState } from 'react';
+import DataTable from 'react-data-table-component';
+import { Header } from '../../Layouts/Header/index'
+import data from '../data'
 
-export const CategoryList : React.FC = ():JSX.Element => {
+export const CategoryList: React.FC = (): JSX.Element => {
+    const columns: any = [
+        {
+            name: "Title",
+            selector: "title",
+            sortable: true,
+        },
+        {
+            name: "Directior",
+            selector: "director",
+            sortable: true,
+        },
+        {
+            name: "Runtime (m)",
+            selector: "runtime",
+            right: true,
+        },
+    ];
+
+    const [records, setRecord] = useState(data);
+
+    function handleFilter(event: any) {
+        const newData = data.filter((row) => {
+            return row.title.toLowerCase().includes(event.target.value.toLowerCase());
+        });
+        setRecord(newData);
+    }
+
     return <>
+        {/* header */}
         <Header title='Category list' another_page_title='add' another_page_link='/category/create' ></Header>
+
+        {/* category list */}
+        <section className='mt-4'>
+            <div className="text-end">
+                <input
+                    type="text"
+                    className="border border-gray-400 py-1 rounded-md px-3"
+                    placeholder="search"
+                    onChange={handleFilter}
+                    id=""
+                />
+            </div>
+            <DataTable
+                data={records}
+                columns={columns}
+                pagination
+                fixedHeader
+            ></DataTable>
+        </section>
+
     </>
 }
